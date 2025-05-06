895bdadc3583fd6a3e3599e90bed92e135bbb5a0
Camel-app <camel.app@proton.me>
2025-05-06 16:41:49 +0200

    update web probing folder structure

---
+++ Analyses/2_webProbing/src/API_key_adjust.py
@@ -0,0 +1,12 @@
+"""
+API-Key Hugging Face (https://huggingface.co/settings/tokens)
+"""
+
+# hugging face keys:
+hugging_api_key = "your_hugging_face_key"
+
+"""
+API-Key OpenAI (https://platform.openai.com/api-keys)
+"""
+# openAI key:
+openAI_key = "your_openAI_key"
